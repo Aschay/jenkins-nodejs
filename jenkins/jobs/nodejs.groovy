a5885e750bfe2acf371ba4fdd7de1b1114f8e2d0
Aschay <ayadi.chayma@gmail.com>
2021-03-12 14:22:39 +0100

    adding job

---
+++ jenkins/jobs/nodejs.groovy
@@ -0,0 +1,18 @@
+job('simple-nodejs-jenkins') {
+    scm {
+        git('https://github.com/Aschay/simple-nodejs-jenkins.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
