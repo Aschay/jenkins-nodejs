90514216d368f8c79c2f5d0074cf8304ca828048
Aschay <ayadi.chayma@gmail.com>
2021-03-15 15:45:50 +0100

    fix config

--- config/config-db.js
+++ config/config-db.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: 3000,
     db: {
-      production: "mongodb://user:pass@example.com:1234/musika",
+      production: "mongodb://user:pass@example.com:1234/nodedb",
       development: "mongodb://localhost/nodedb",
       test: "mongodb://localhost:27017/nodedb",
     },
